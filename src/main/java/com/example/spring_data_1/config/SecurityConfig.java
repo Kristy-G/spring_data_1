6816a217511d62063ff1fbb2e753bd060867f66d
Kristy-G <69619683+Kristy-G@users.noreply.github.com>
2022-01-03 23:14:40 +0700

    Lesson_11

---
+++ src/main/java/com/example/spring_data_1/config/SecurityConfig.java
@@ -0,0 +1,13 @@
+package com.example.spring_data_1.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(securedEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+}
---
+++ src/main/java/com/example/spring_data_1/entity/User.java
@@ -0,0 +1,28 @@
+package com.example.spring_data_1.entity;
+
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column
+    private Long id;
+    @Column(name = "username")
+    private String name;
+    @Column(name = "password")
+    private String password;
+
+    @ManyToMany
+    @JoinTable(
+            name = "users_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+}
